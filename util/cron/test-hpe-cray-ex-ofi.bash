f1805cd3e65a51586736efeabf60d172867fe54a
Ben Harshbarger <ben.harshb@gmail.com>
2025-07-01 11:44:07 -0700

    custom script for debugging
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- util/cron/test-hpe-cray-ex-ofi.bash
+++ util/cron/test-hpe-cray-ex-ofi.bash
@@ -12,9 +12,13 @@ export CHPL_RT_COMM_OFI_EXPECTED_PROVIDER="cxi"
 export CHPL_RT_MAX_HEAP_SIZE=16g
 export SLURM_PARTITION=allnodes 
 
+echo "benharsh modified cronscript"
 
+export CHPL_NIGHTLY_LOGDIR=/tmp/test-chapel-nightly-debug/cronlog
 # test a small subset of all tests due to limited resources
 # ideally, we should run the whole suite
-export CHPL_NIGHTLY_TEST_DIRS="release/examples/ runtime/configMatters/ multilocale/"
+export CHPL_NIGHTLY_TEST_DIRS="multilocale/deitz/needMultiLocales/"
+#export CHPL_START_TEST_ARGS="multilocale/deitz/needMultiLocales/raCommCheckLCG.chpl -numtrials 3"
 
-$UTIL_CRON_DIR/nightly -cron -blog ${nightly_args}
+
+$UTIL_CRON_DIR/nightly -cron ${nightly_args}
